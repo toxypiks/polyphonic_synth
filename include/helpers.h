2c6134af34fee765e04432b22125ebbac30d1933
toxypiks <HannaHinkelbein89@gmx.de>
2025-03-22 12:58:22 +0100

    Add helpful macros

--- include/helpers.h
+++ include/helpers.h
@@ -1,6 +1,18 @@
 #ifndef HELPERS_H_
 #define HELPERS_H_
 
+#define UNUSED(x) (void)(x)
+#define UNIMPLEMENTED(message) \
+    do { \
+        fprintf(stderr, "%s:%d: UNIMPLEMENTED: %s\n", __FILE__, __LINE__, message); \
+        exit(1); \
+    } while (0)
+#define UNREACHABLE(message) \
+    do { \
+        fprintf(stderr, "%s:%d: UNREACHABLE: %s\n", __FILE__, __LINE__, message); \
+        exit(1); \
+    } while (0)
+
 #define MAX(a,b) \
   ({ __typeof__ (a) _a = (a); \
       __typeof__ (b) _b = (b); \
